ee630082d3ca5b539734bd8e3cd6325e7b04f8cc
Slaven Hrkac <shrkac@sh-mbp.local>
2020-06-15 19:32:07 +0200

    add daily wiki tool

---
+++ daily_wiki/daily_wiki/spiders/article.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from daily_wiki.items import Article
+
+class ArticleSpider(scrapy.Spider):
+    name = 'article'
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = ['https://en.wikipedia.org/wiki/Wikipedia:Featured_articles']
+
+    # Enable Feed Storage
+    custom_settings = {
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'file:///tmp/featured-articles-%(time)s.json'
+    }
+    
+    def parse(self, response):
+        host = self.allowed_domains[0]
+        for link in response.css(".featured_article_metadata > a"):
+            yield Article(
+                title = link.attrib.get("title"),
+                link = f"https://{host}{link.attrib.get('href')}"
+            )
